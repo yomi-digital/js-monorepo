c906520ef3c07ebea5f2eb56a2fa9cf2aeb3440b
Steve Rogers <stefano@yomi.digital>
2023-08-01 09:38:38 +0200

    min fix kwenta icon

--- v2/perps-v2/ui/src/components/Icons/KwentaIcon/KwentaIcon.tsx
+++ v2/perps-v2/ui/src/components/Icons/KwentaIcon/KwentaIcon.tsx
@@ -1,95 +1,85 @@
 import { Icon, IconProps } from '@chakra-ui/icons';
 
-export const KwentaIcon = ({
-  width = '30px',
-  height = '30px',
-  ...props
-}: IconProps) => (
-  <Icon width={width} height={height}  fill="none" {...props}>
-    <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="8.5" cy="8.5" r="8" fill="url(#paint0_linear_26_5654)" stroke="#0B0B22" />
-      <g clip-path="url(#clip0_26_5654)">
-        <path
-          d="M8.69336 3.1875L14.1863 6.5983V10.0091L8.69336 6.5983V3.1875Z"
-          fill="url(#paint1_linear_26_5654)"
-        />
-        <path
-          d="M8.69336 13.7989L14.1863 10.4225V7.04617L8.69336 10.4225V13.7989Z"
-          fill="url(#paint2_linear_26_5654)"
-        />
-        <path
-          d="M8.69327 3.1875L3.33594 6.5983V10.0091L8.69327 6.5983V3.1875Z"
-          fill="url(#paint3_linear_26_5654)"
-        />
-        <path
-          d="M8.69327 13.7989L3.33594 10.4225V7.04617L8.69327 10.4225V13.7989Z"
-          fill="url(#paint4_linear_26_5654)"
-        />
-      </g>
+export const KwentaIcon = ({ width = '25px', height = '25px', ...props }: IconProps) => (
+  <Icon width={width} height={height} fill="none" {...props}>
+    <svg width="25" height="25" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M13.5 26C20.4036 26 26 20.4036 26 13.5C26 6.59644 20.4036 1 13.5 1C6.59644 1 1 6.59644 1 13.5C1 20.4036 6.59644 26 13.5 26Z"
+        fill="url(#paint0_linear_328_22)"
+        stroke="#0B0B22"
+      />
+      <path
+        d="M13.8022 5.19922L22.385 10.5286V15.858L13.8022 10.5286V5.19922Z"
+        fill="url(#paint1_linear_328_22)"
+      />
+      <path
+        d="M13.8022 21.7797L22.385 16.504V11.2285L13.8022 16.504V21.7797Z"
+        fill="url(#paint2_linear_328_22)"
+      />
+      <path
+        d="M13.802 5.19922L5.43115 10.5286V15.858L13.802 10.5286V5.19922Z"
+        fill="url(#paint3_linear_328_22)"
+      />
+      <path
+        d="M13.802 21.7797L5.43115 16.504V11.2285L13.802 16.504V21.7797Z"
+        fill="url(#paint4_linear_328_22)"
+      />
       <defs>
         <linearGradient
-          id="paint0_linear_26_5654"
-          x1="3.4"
-          y1="1.98333"
-          x2="14.1667"
-          y2="17"
+          id="paint0_linear_328_22"
+          x1="5.53125"
+          y1="3.3177"
+          x2="22.3542"
+          y2="26.7813"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#835E34" />
-          <stop offset="1" stop-color="#344548" />
+          <stop stopColor="#835E34" />
+          <stop offset="1" stopColor="#344548" />
         </linearGradient>
         <linearGradient
-          id="paint1_linear_26_5654"
-          x1="18.8703"
-          y1="6.57928"
-          x2="5.32498"
-          y2="6.57928"
+          id="paint1_linear_328_22"
+          x1="29.7037"
+          y1="10.4989"
+          x2="8.53915"
+          y2="10.4989"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#996939" />
-          <stop offset="1" stop-color="#D0A875" />
+          <stop stopColor="#996939" />
+          <stop offset="1" stopColor="#D0A875" />
         </linearGradient>
         <linearGradient
-          id="paint2_linear_26_5654"
-          x1="11.4398"
-          y1="13.7989"
-          x2="12.8345"
-          y2="0.889045"
+          id="paint2_linear_328_22"
+          x1="18.0936"
+          y1="21.7797"
+          x2="20.2728"
+          y2="1.60801"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#272728" />
-          <stop offset="1" stop-color="#4B4B4B" />
+          <stop stopColor="#272728" />
+          <stop offset="1" stopColor="#4B4B4B" />
         </linearGradient>
         <linearGradient
-          id="paint3_linear_26_5654"
-          x1="8.72635"
-          y1="4.58593"
-          x2="3.24266"
-          y2="8.78682"
+          id="paint3_linear_328_22"
+          x1="13.8537"
+          y1="7.38427"
+          x2="5.2854"
+          y2="13.9482"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#996939" />
-          <stop offset="1" stop-color="#D0A875" />
+          <stop stopColor="#996939" />
+          <stop offset="1" stopColor="#D0A875" />
         </linearGradient>
         <linearGradient
-          id="paint4_linear_26_5654"
-          x1="6.0146"
-          y1="13.7989"
-          x2="6.0146"
-          y2="7.04617"
+          id="paint4_linear_328_22"
+          x1="9.61656"
+          y1="21.7797"
+          x2="9.61656"
+          y2="11.2285"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#363636" />
-          <stop offset="1" stop-color="#333232" />
+          <stop stopColor="#363636" />
+          <stop offset="1" stopColor="#333232" />
         </linearGradient>
-        <clipPath id="clip0_26_5654">
-          <rect
-            width="12.2188"
-            height="10.625"
-            fill="white"
-            transform="translate(2.65625 3.1875)"
-          />
-        </clipPath>
       </defs>
     </svg>
   </Icon>
